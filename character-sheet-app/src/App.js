7f2d974536dba634cb5e93eac5bb6ff1c7360782
Alden Davidson <adavidson+github@protonmail.ch>
2021-08-28 17:48:05 -0700

    added button to test darkmode

--- character-sheet-app/src/App.js
+++ character-sheet-app/src/App.js
@@ -1,5 +1,34 @@
 function App() {
-  return <div className="App">Hello there!</div>;
+  const toggleDarkMode = () => {
+    console.log(localStorage.theme);
+    if (localStorage.theme === undefined) {
+      localStorage.theme = "dark";
+    }
+    localStorage.theme === "light"
+      ? (localStorage.theme = "dark")
+      : (localStorage.theme = "light");
+
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme:dark)").matches)
+    ) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  };
+
+  return (
+    <div className="App">
+      <div className="h-screen w-screen flex flex-col justify-center items-center bg-white dark:bg-gray-800">
+        Hello there!
+        <button onClick={toggleDarkMode} className="text-black dark:text-white">
+          ACTIVATE DARK MODE
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
